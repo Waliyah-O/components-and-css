80d10a91cb44ed6d66499dac262e090a30c22de7
Waliyah Okunade <129737697+Waliyah-O@users.noreply.github.com>
2024-10-25 17:17:01 +0100

    Create Search.tsx

---
+++ context/Search.tsx
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState } from "react";
+
+const SearchContext = createContext({
+  searchTerm: "",
+  setSearchTerm: (term: string) => {},
+});
+
+export const SearchProvider = ({ children }: { children: React.ReactNode }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <SearchContext.Provider value={{ searchTerm, setSearchTerm }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export const useSearch = () => useContext(SearchContext);
